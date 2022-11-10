345f7cfa5d9e87eb16e657684572db2c1ae86f14
Eugene Stsefankou <stefjen@mail.ru>
2022-11-10 13:26:39 +0300

    Initial Commit

---
+++ RadioComm/RadioCommApp.swift
@@ -0,0 +1,17 @@
+//
+//  RadioCommApp.swift
+//  RadioComm
+//
+//  Created by Евгений on 10.11.22.
+//
+
+import SwiftUI
+
+@main
+struct RadioCommApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
